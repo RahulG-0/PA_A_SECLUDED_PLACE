b0d0b4d28481f4e1d19e17408bfbb363d228d5bc
rockisking1212 <rockisking7@gmail.com>
2023-01-14 19:11:18 -0500

    stuff

--- src/GameView.java
+++ src/GameView.java
@@ -13,11 +13,7 @@ public class GameView extends JPanel {
     private GameModel gameModel; // Instance of model
     private TitleModel titleModel;
 
-<<<<<<< HEAD
-    private JLabel background = new JLabel();
-=======
     private JLabel backGround = new JLabel();
->>>>>>> 8e31def5ee586fce3606e882f9345447d2ae80ec
 
     private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
@@ -44,23 +40,13 @@ public class GameView extends JPanel {
         int width = (int)this.screenSize.getWidth();
         int height = (int)this.screenSize.getHeight();
 
-<<<<<<< HEAD
-=======
         System.out.println(height);
 
->>>>>>> 8e31def5ee586fce3606e882f9345447d2ae80ec
         this.setBounds(0,0,width,height);
 
         this.defend.setBounds(100, 400, 30, 40);
 
         // setting the colour of the backround
-<<<<<<< HEAD
-        background.setBounds(0,0,width,height);
-        background.setForeground(new Color(255,255,0));
-
-        this.setBackground(new Color(255, 255, 255));
-
-=======
         backGround.setBounds(0,0,width,height);
         // backGround.setBorder(getBorder());
         backGround.setBackground(Color.BLACK);
@@ -80,7 +66,6 @@ public class GameView extends JPanel {
         this.add(playerHealth);
         this.add(MonsterHealth);
         this.add(backGround);
->>>>>>> 8e31def5ee586fce3606e882f9345447d2ae80ec
     }
 
     // Registers controllers
