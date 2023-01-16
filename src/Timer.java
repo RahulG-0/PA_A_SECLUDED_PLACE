adad9a8f6deb8fdc03aa69ed408cba9f1d7d7ae8
AnirudhBharadwaj <4n1ru0h@gmail.com>
2023-01-16 14:53:02 -0500

    Added stuff and kept my will to live

---
+++ src/Timer.java
@@ -0,0 +1,12 @@
+// Program Name: Timer
+// Last Modified:
+// Name: Rahul Gurukiran & Anirudh Bharadwaj
+// Description: Runs a timer whenever required
+
+// Imports
+
+public class Timer {
+    public double startTime = 0.00;
+    public double currentTime = 0.00;
+    private Timer timer = new Timer();
+}
