018b531bb3560e5f874c4444e22fd3236affa1c5
rockisking1212 <rockisking7@gmail.com>
2023-01-19 23:57:20 -0500

    stuffs

--- src/buttonGameController.java
+++ src/buttonGameController.java
@@ -4,13 +4,15 @@ import javax.swing.JButton;
 
 public class buttonGameController implements ActionListener {
     private GameModel gameModel;
+    TitleModel titleModel;
     private MusicPlayer mPlayer;
     private JButton aButton;
 
     // Constructor
-    public buttonGameController(GameModel gameModel, JButton aButton) {
+    public buttonGameController(GameModel gameModel,TitleModel titleModel, JButton aButton) {
         this.gameModel = gameModel;
         this.mPlayer = new MusicPlayer();
+        this.titleModel = titleModel;
         this.aButton = aButton;
     }
 
@@ -27,15 +29,23 @@ public class buttonGameController implements ActionListener {
 
         }
 
+        if(((JButton)aButton).toString().contains("text=Quit_Game")){
+
+            gameModel.displayOptionsPannel = false;
+            titleModel.startGame = false;
+            titleModel.gameDifficulty = "";
+
+        }
+
         for(int i = 0; i<18;i++){
             if(((JButton)aButton).toString().contains("text="+(i))){
                 gameModel.buttonVisible[i] = false;
-                // System.out.println("bob");
                 gameModel.amountClicked++;
             }
         }
         
         this.mPlayer.buttonSound();
         gameModel.update();
+
     }
 }
